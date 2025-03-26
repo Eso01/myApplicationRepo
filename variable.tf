9d6e75d023182301a7ddd85987a481f0170e9324
eso01 <samuelonwuachi88@gmail.com>
2025-03-26 21:46:49 +0000

    update ec2 key

--- variable.tf
+++ variable.tf
@@ -24,5 +24,5 @@ variable "tag_name" {
 } 
 variable "ami_id" { 
     description = "AMI for Ubuntu Ec2 instance" 
-    default     = "ami-04b4f1a9cf54c11d0" 
+    default     = "ami-084568db4383264d4" 
 }
